81048b705d046297c8c06993bb711a56230f51f7
samwhelp <samwhelp@users.noreply.github.com>
2025-08-14 22:53:02 +0800

    fix

--- project/by-theme/core/engine/helper/ext/master/sys/repo.sh
+++ project/by-theme/core/engine/helper/ext/master/sys/repo.sh
@@ -817,3 +817,168 @@ sys_repo_commit_and_push_gh_page () {
 ##
 ### Tail: Master / Sys / Repo / Push
 ################################################################################
+
+
+
+
+################################################################################
+### Head: Master / Sys / Repo / Overlay
+##
+
+sys_repo_overlay_main () {
+
+	local source_dir_path="${REF_MASTER_TMP_DIR_PATH}/fix"
+	local target_dir_path
+
+	local prefix_name="${REF_REPO_PREFIX_NAME}"
+	local work_dir_path="${REF_WORK_DIR_PATH}"
+	local theme_list="${REF_ENTITY_THEME_LIST}"
+	local desktop_list="${REF_ENTITY_DESKTOP_LIST}"
+
+
+	local the_theme_name
+	local the_desktop_name
+	local the_first_level_dir_path
+	local the_second_level_dir_path
+	local the_second_level_dir_name
+
+
+	echo
+	echo mkdir -p "${work_dir_path}"
+	echo
+	mkdir -p "${work_dir_path}"
+
+
+	for the_theme_name in ${theme_list}; do
+		#echo "${the_theme_name}"
+
+		the_first_level_dir_path="${work_dir_path}/${the_theme_name}"
+
+		echo
+		echo mkdir -p "${the_first_level_dir_path}"
+		echo
+		mkdir -p "${the_first_level_dir_path}"
+
+		for the_desktop_name in ${desktop_list}; do
+
+			the_second_level_dir_name="${prefix_name}-${the_desktop_name}-${the_theme_name}"
+			the_second_level_dir_path="${the_first_level_dir_path}/${the_second_level_dir_name}"
+
+			echo
+			echo mkdir -p "${the_second_level_dir_path}"
+			echo
+			mkdir -p "${the_second_level_dir_path}"
+
+
+
+
+			target_dir_path="${the_second_level_dir_path}/${the_second_level_dir_name}-main"
+
+
+			echo
+			echo mkdir -p "${source_dir_path}"
+			echo
+			mkdir -p "${source_dir_path}"
+
+
+			echo
+			echo mkdir -p "${target_dir_path}"
+			echo
+			mkdir -p "${target_dir_path}"
+
+
+			echo
+			echo cp -rfT "${source_dir_path}" "${target_dir_path}"
+			echo
+			cp -rfT "${source_dir_path}" "${target_dir_path}"
+
+
+
+
+		done
+	done
+
+
+	return 0
+}
+
+sys_repo_overlay_gh_page () {
+
+	local source_dir_path="${REF_MASTER_TMP_DIR_PATH}/fix"
+	local target_dir_path
+
+	local prefix_name="${REF_REPO_PREFIX_NAME}"
+	local work_dir_path="${REF_WORK_DIR_PATH}"
+	local theme_list="${REF_ENTITY_THEME_LIST}"
+	local desktop_list="${REF_ENTITY_DESKTOP_LIST}"
+
+
+	local the_theme_name
+	local the_desktop_name
+	local the_first_level_dir_path
+	local the_second_level_dir_path
+	local the_second_level_dir_name
+
+
+	echo
+	echo mkdir -p "${work_dir_path}"
+	echo
+	mkdir -p "${work_dir_path}"
+
+
+	for the_theme_name in ${theme_list}; do
+		#echo "${the_theme_name}"
+
+		the_first_level_dir_path="${work_dir_path}/${the_theme_name}"
+
+		echo
+		echo mkdir -p "${the_first_level_dir_path}"
+		echo
+		mkdir -p "${the_first_level_dir_path}"
+
+		for the_desktop_name in ${desktop_list}; do
+
+			the_second_level_dir_name="${prefix_name}-${the_desktop_name}-${the_theme_name}"
+			the_second_level_dir_path="${the_first_level_dir_path}/${the_second_level_dir_name}"
+
+			echo
+			echo mkdir -p "${the_second_level_dir_path}"
+			echo
+			mkdir -p "${the_second_level_dir_path}"
+
+
+
+
+			target_dir_path="${the_second_level_dir_path}/${the_second_level_dir_name}-gh-pages"
+
+
+			echo
+			echo mkdir -p "${source_dir_path}"
+			echo
+			mkdir -p "${source_dir_path}"
+
+
+			echo
+			echo mkdir -p "${target_dir_path}"
+			echo
+			mkdir -p "${target_dir_path}"
+
+
+			echo
+			echo cp -rfT "${source_dir_path}" "${target_dir_path}"
+			echo
+			cp -rfT "${source_dir_path}" "${target_dir_path}"
+
+
+
+
+		done
+	done
+
+
+	return 0
+}
+
+##
+### Tail: Master / Sys / Repo / Overlay
+################################################################################
