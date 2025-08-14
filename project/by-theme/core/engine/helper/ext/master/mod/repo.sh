6523be15c46bc4f96c5399cebe6eac3a0716ad61
samwhelp <samwhelp@users.noreply.github.com>
2025-08-14 20:57:24 +0800

    fix

--- project/by-theme/core/engine/helper/ext/master/mod/repo.sh
+++ project/by-theme/core/engine/helper/ext/master/mod/repo.sh
@@ -6,13 +6,7 @@
 
 mod_repo_clone_all () {
 
-	#sys_repo_prepare_dir
-
-	sys_repo_prepare_dir_and_clone_file
-
-	#util_repo_create_clone_script_content "test"
-
-	#util_repo_create_clone_script_file "teat" "/tmp/clone.sh"
+	sys_repo_prepare_dir_and_clone_file_and_clone_repo
 
 	return 0
 }
--- project/by-theme/core/engine/helper/ext/master/sys/repo.sh
+++ project/by-theme/core/engine/helper/ext/master/sys/repo.sh
@@ -105,6 +105,69 @@ sys_repo_prepare_dir_and_clone_file () {
 	return 0
 }
 
+sys_repo_prepare_dir_and_clone_file_and_clone_repo () {
+
+	local prefix_name="ubuntu-iso-builder-import-pacstall"
+	local work_dir_path="${REF_WORK_DIR_PATH}"
+	local theme_list="${REF_ENTITY_THEME_LIST}"
+	local desktop_list="${REF_ENTITY_DESKTOP_LIST}"
+
+
+	local the_theme_name
+	local the_desktop_name
+	local the_first_level_dir_path
+	local the_second_level_dir_path
+	local the_second_level_dir_name
+
+
+	echo
+	echo mkdir -p "${work_dir_path}"
+	echo
+	mkdir -p "${work_dir_path}"
+
+
+	for the_theme_name in ${theme_list}; do
+		#echo "${the_theme_name}"
+
+		the_first_level_dir_path="${work_dir_path}/${the_theme_name}"
+
+		echo
+		echo mkdir -p "${the_first_level_dir_path}"
+		echo
+		mkdir -p "${the_first_level_dir_path}"
+
+		for the_desktop_name in ${desktop_list}; do
+
+			the_second_level_dir_name="${prefix_name}-${the_desktop_name}-${the_theme_name}"
+			the_second_level_dir_path="${the_first_level_dir_path}/${the_second_level_dir_name}"
+
+			echo
+			echo mkdir -p "${the_second_level_dir_path}"
+			echo
+			mkdir -p "${the_second_level_dir_path}"
+
+			echo
+			echo "create file: ${the_second_level_dir_path}/clone.sh"
+			echo
+			util_repo_create_clone_script_file "${the_second_level_dir_name}" "${the_second_level_dir_path}/clone.sh"
+
+
+			cd "${the_second_level_dir_path}"
+
+			echo
+			echo "clone repo: ${the_second_level_dir_path}/clone.sh"
+			echo
+			sh ./clone.sh
+
+			cd "${OLDPWD}"
+
+		done
+	done
+
+
+	return 0
+}
+
 
 ##
 ### Tail: Master / Sys / Repo
